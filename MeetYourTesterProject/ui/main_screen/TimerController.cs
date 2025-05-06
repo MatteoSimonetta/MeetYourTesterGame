127c41c105e9b07712a6a6762dba0b59efab08c1
Matteo Simonetta <matteosimonetta@outlook.com>
2025-05-07 00:00:30 +0200

    break: created C# for timer controller - no problems (NB. START CHECKING FROM INITCOUNTDOWN BREAK CHANGES IF FUTURE PROBLEMS ARE DETECTED

---
+++ MeetYourTesterProject/ui/main_screen/TimerController.cs
@@ -0,0 +1,49 @@
+using Godot;
+using System;
+
+public partial class TimerController : Node
+{
+    private Node progressBar;
+    private Node mainGameScene;
+    private Timer timerNode;
+
+    public override void _Ready()
+    {
+        progressBar = GetNode("../../../ProgressBarControl");
+        mainGameScene = GetNode("../../..");
+        timerNode = GetNode<Timer>("TimerNode");
+
+        mainGameScene.Connect("game_pause_changed", new Callable(this, nameof(CatchPauseChanged)));
+    }
+
+    // This function is assumed to be connected in the editor to the timeout() signal of TimerNode
+    private void OnTimerNodeTimeout()
+    {
+        Globals.Instance.GameTime += 1;
+        progressBar.Call("auto_increment");
+        this.Set("text", Utils.FloatToTime(Globals.Instance.GameTime));
+    }
+
+    private void CatchPauseChanged()
+    {
+        if (Globals.Instance.GamePaused)
+            CatchPause();
+        else
+            CatchUnpause();
+    }
+
+    private void CatchPause()
+    {
+        timerNode.Paused = true;
+    }
+
+    private void CatchUnpause()
+    {
+        timerNode.Paused = false;
+    }
+
+    public void CatchSpeedChange()
+    {
+        Engine.TimeScale = Globals.Instance.GameSpeed;
+    }
+}
