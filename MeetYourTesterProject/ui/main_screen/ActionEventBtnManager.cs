852f7d48843a500aa8637b2bce069f3ec95383e6
Matteo Simonetta <matteosimonetta@outlook.com>
2025-06-24 11:18:08 +0200

    M003: fixed hexagon disabled when one is selected

--- MeetYourTesterProject/ui/main_screen/ActionEventBtnManager.cs
+++ MeetYourTesterProject/ui/main_screen/ActionEventBtnManager.cs
@@ -10,8 +10,10 @@ public partial class ActionEventBtnManager : TextureButton
     
     private Texture2D backupDisableImage;
     private Texture2D backupHoverImage;
+    private Texture2D backupNormalImage; // Store original normal texture
     private bool isActionEventGenerated = false;
     private Node timerChild = null;
+    private bool isTemporarilyDisabled = false; // New flag for temporary disable
     
     // Access to autoloaded GDScript singleton
     private Node globals;
@@ -29,10 +31,18 @@ public partial class ActionEventBtnManager : TextureButton
         
         backupDisableImage = TextureDisabled;
         backupHoverImage = TextureHover;
+        backupNormalImage = TextureNormal; // Store original normal texture
         timerChild = GetChild(0).GetChild(0);
         
         // Connect the pressed signal
         Pressed += OnPressed;
+        
+        // Connect to terminal's answer signal to re-enable hexagons
+        var terminal = GetNode("../../Terminal/_terminal_mock/terminal_content");
+        if (terminal != null)
+        {
+            terminal.Connect("AnswerSignal", new Callable(this, nameof(OnAnswerGiven)));
+        }
     }
 
     public override void _Process(double delta)
@@ -41,12 +51,12 @@ public partial class ActionEventBtnManager : TextureButton
 
     private void _on_timer_timeout()
     {
-        if (!isActionEventGenerated)
+        if (!isActionEventGenerated && !isTemporarilyDisabled)
         {
             StartSoundSpawnEvent();
             GenerateActionEvent();
         }
-        else
+        else if (isActionEventGenerated && !isTemporarilyDisabled)
         {
             RemoveActionEvent();
         }
@@ -54,6 +64,9 @@ public partial class ActionEventBtnManager : TextureButton
 
     private void OnPressed()
     {
+        // Only allow pressing if not temporarily disabled
+        if (isTemporarilyDisabled) return;
+        
         // To switch texture, we first save the disabled one, then replace it with the pressed one
         TextureDisabled = TexturePressed;
         Disabled = true;
@@ -65,15 +78,111 @@ public partial class ActionEventBtnManager : TextureButton
             { "node_name", nodeName }
         };
         
+        // Disable all other hexagons
+        DisableAllOtherHexagons();
+        
         // Emit signal that this button has been pressed
         EmitSignal(SignalName.HexagonClicked, parameters);
         StartSoundClickedEvent();
     }
 
+    private void DisableAllOtherHexagons()
+    {
+        // Get all hexagon nodes (adjust paths according to your scene structure)
+        var hexagonNodes = new string[] 
+        { 
+            "Database", 
+            "Delivery", 
+            "Business_Logic", 
+            "Backend", 
+            "UI_UX" 
+        };
+        
+        foreach (string hexagonName in hexagonNodes)
+        {
+            var hexagonNode = GetNode("../" + hexagonName);
+            if (hexagonNode != null && hexagonNode != this.GetParent())
+            {
+                hexagonNode.Call("SetTemporaryDisable", true);
+            }
+        }
+    }
+
+    private void OnAnswerGiven(Godot.Collections.Dictionary answerData)
+    {
+        // Re-enable all hexagons when an answer is given
+        EnableAllHexagons();
+    }
+
+    private void EnableAllHexagons()
+    {
+        // Get all hexagon nodes
+        var hexagonNodes = new string[] 
+        { 
+            "Database", 
+            "Delivery", 
+            "Business_Logic", 
+            "Backend", 
+            "UI_UX" 
+        };
+        
+        foreach (string hexagonName in hexagonNodes)
+        {
+            var hexagonNode = GetNode("../" + hexagonName);
+            if (hexagonNode != null)
+            {
+                hexagonNode.Call("SetTemporaryDisable", false);
+            }
+        }
+    }
+
+    // Public method to be called by other hexagons
+    public void SetTemporaryDisable(bool disable)
+    {
+        isTemporarilyDisabled = disable;
+        
+        if (disable)
+        {
+            // If this hexagon has an active action event, disable it temporarily
+            if (isActionEventGenerated)
+            {
+                Disabled = true;
+                // Optionally change texture to show it's temporarily disabled
+                TextureNormal = TextureDisabled;
+            }
+        }
+        else
+        {
+            // Re-enable if it had an action event
+            if (isActionEventGenerated)
+            {
+                Disabled = false;
+                // Restore the original normal texture (the white/clickable texture)
+                // We need to restore the texture that was set when the action event was generated
+                // This should be the original TextureNormal before any modifications
+                RestoreOriginalTextures();
+            }
+        }
+    }
+
+    private void RestoreOriginalTextures()
+    {
+        // Restore textures to their original state when action event is active
+        if (isActionEventGenerated)
+        {
+            // Restore the white/clickable texture that indicates an active action event
+            TextureNormal = backupNormalImage; // This should be the white texture
+            TextureHover = backupHoverImage;
+        }
+    }
+
     // Functions to handle changes of state for the button
     private void GenerateActionEvent()
     {
-        Disabled = false;
+        if (!isTemporarilyDisabled)
+        {
+            Disabled = false;
+        }
         isActionEventGenerated = true;
         
         if (timerChild != null)
@@ -88,6 +197,7 @@ public partial class ActionEventBtnManager : TextureButton
     public void RemoveActionEvent()
     {
         TextureDisabled = backupDisableImage;
+        TextureNormal = backupNormalImage; // Restore original white texture
         Disabled = true;
         isActionEventGenerated = false;
         
